a8c5e84985e1224a7fa2392f8f2d085fbf236f72
akhil-s-2004 <148994185+akhil-s-2004@users.noreply.github.com>
2024-04-16 11:19:39 +0530

    Create pgm1.c

---
+++ process communication/pgm1.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 1000
+
+struct message
+{
+    int flag;
+    int turn;
+    int user1_up;
+    int user2_up;
+    char msg[SIZE];
+};
+struct message* mptr;
+
+int main()
+{
+    int fd;
+    fd=shm_open("Akhi", O_RDWR, 0666);
+    if (fd == -1)
+    {
+        perror("shm_open");
+        exit(1);
+    }
+
+    mptr=mmap(mptr, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,fd,0);
+    if(mptr == MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+    mptr->flag=1;
+    mptr->turn=2;
+    while(mptr->flag == 1)
+    {
+        while(mptr->turn != 2);
+        if(mptr->user1_up == 1)
+        {
+            printf("Message from USER1 : %s\n",mptr->msg);
+            mptr->user1_up=0;
+        }
+        if(mptr->user2_up == 0 && mptr->user1_up != 1)
+        {
+            printf("Enter the message (USER2) : ");
+            scanf(" %[^\n]",mptr->msg);
+            if(strcmp(mptr->msg,"stop") == 0)
+            {
+                mptr->flag=0;
+            }
+            mptr->user2_up=1;
+        }
+        mptr->turn=1;
+    }
+    if(munmap(mptr, SIZE) == -1)
+    {
+        perror("munmap");
+        exit(1);
+    }
+
+    close(fd);
+    return 0;
+}
